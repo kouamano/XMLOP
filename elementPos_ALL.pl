32f90ceeb5ae662f9556d7c57a67373b3d69c2e5
kouamano <amano.au1@gmail.con>
2014-03-02 14:55:45 +0900

    new

---
+++ elementPos_ALL.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+while(<>){
+	push(@arr,$_);
+}
+$tx = join("",@arr);
+while($tx =~ /(<[^<>]*>)/ig){
+	$m = "";
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	#$1 =~ /(sec-type=\"[^\"]\")/ig;
+	print "$s-$e:s:$1:\n";
+}
+print ";;\n";
+while($tx =~ /(<\/[^<>]*>)/ig){
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	print "$s-$e:e:\n";
+}
+print ";;;\n";
