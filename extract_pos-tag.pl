f66cef4f15349718bc022f4190c182ccd126995b
kouamano <amano.au1@gmail.con>
2014-03-02 15:41:01 +0900

    new

---
+++ extract_pos-tag.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+while(<>){
+	push(@arr,$_);
+}
+$tx = join("",@arr);
+while($tx =~ /(<[^<>]*>)/ig){
+	$m = "";
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	print "$s-$e:$1:;\n";
+}
+print ";;;\n";
