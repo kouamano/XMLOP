1df23fd1b256d4e6b71950c84c4a51a638952055
kouamano <amano.au1@gmail.com>
2014-09-03 11:51:35 +0900

    new

---
+++ elementPos_ALL+b_sort.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+while(<>){
+	push(@arr,$_);
+}
+$tx = join("",@arr);
+@arr = ();
+while($tx =~ /(<[^<>\/!]*\>)/ig){
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	#print "$s-$e:s:$1:\n";
+	push(@arr,"$s-$e:s:$1:\n");
+}
+#print ";;\n";
+while($tx =~ /(<\/[^<>!]*>)/ig){
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	#print "$s-$e:e:$1:\n";
+	push(@arr,"$s-$e:e:$1:\n");
+}
+while($tx =~ /(<[^<>\/!]*\/\>)/ig){
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	#print "$s-$e:b:$1:\n";
+	push(@arr,"$s-$e:b:$1:\n");
+}
+#print ";;;\n";
+@arr = sort {$a <=> $b} @arr;
+print @arr;
+print ";;;\n";
