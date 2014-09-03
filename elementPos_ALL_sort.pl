25ccffdf3e362b1866f53929ce4b884148a4d78f
kouamano <amano.au1@gmail.com>
2014-09-03 11:37:48 +0900

    rev

--- elementPos_ALL_sort.pl
+++ elementPos_ALL_sort.pl
@@ -22,6 +22,14 @@ while($tx =~ /(<\/[^<>!]*>)/ig){
 	#print "$s-$e:e:$1:\n";
 	push(@arr,"$s-$e:e:$1:\n");
 }
+while($tx =~ /(<[^<>\/!]*\/\>)/ig){
+	$p = pos($tx);
+	$l = length($1);
+	$s = $p-$l;
+	$e = $p-1;
+	#print "$s-$e:b:$1:\n";
+	push(@arr,"$s-$e:b:$1:\n");
+}
 #print ";;;\n";
 @arr = sort {$a <=> $b} @arr;
 print @arr;
