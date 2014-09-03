a5192f069c94c19b85de2ed6bbc34c151d65c1e8
kouamano <amano.au1@gmail.com>
2014-09-03 15:27:42 +0900

    rev

--- elementPos_ALL.pl
+++ elementPos_ALL.pl
@@ -4,13 +4,11 @@ while(<>){
 	push(@arr,$_);
 }
 $tx = join("",@arr);
-#while($tx =~ /(<[^<>\/]*\/{0,1}>)/ig){
-while($tx =~ /(<[^<>\/]*>)/ig){
+while($tx =~ /(<[^<>]*[^\/]>)/ig){
 	$p = pos($tx);
 	$l = length($1);
 	$s = $p-$l;
 	$e = $p-1;
-	#$1 =~ /(sec-type=\"[^\"]\")/ig;
 	print "$s-$e:s:$1:\n";
 }
 print ";;\n";
