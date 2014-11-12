d0a8154ab4ac323766ef4163b69903aff49caa67
kouamano <amano.au1@gmail.com>
2014-11-12 11:10:49 +0900

    rev

--- merge_region.pl
+++ merge_region.pl
@@ -85,32 +85,56 @@ foreach(@fcont){
 	$lcount = 0;
 	foreach(@arr){
 		#print "f:$fcount l:$lcount\n";
+		$_ =~ s/^\s*//;
+		$_ =~ s/\s*$//;
 		$allcont[$lcount][$fcount] = $_;
 		$lcount++;
 	}
 	$fcount++;
 }
 #print "$lcount\n";
-print "0\n";
-print $allcont[18][0];
-print "1\n";
-print $allcont[18][1];
-print "\n++++++++\n";
-for($i=0;$i<$lcount;$i++){
+#print "0\n";
+#print $allcont[18][0];
+#print "1\n";
+#print $allcont[18][1];
+#print "\n++++++++\n";
+for($i=0;$i<$lcount-1;$i++){
 	@arr = ();
 	for($j=0;$j<$fcount;$j++){
-		print "[$i][$j]\n";
-		print "$allcont[$i][$j]\n";
+		#print "[$i][$j]";
+		#print "$allcont[$i][$j]";
 		@tmp = split("\n",$allcont[$i][$j]);
-		shift(@tmp);
+		$title = shift(@tmp);
 		push(@arr,@tmp);
 	}
-	print "\n -> [[[ @arr ]]]";
+	print "[$i]$title\n";
+	#print "\n -> [[[ @arr ]]]";
 	@maparr = map {(split(":",$_))[1]} @arr;
-	print "\n -> [[[ @maparr ]]]";
+	#print "\n -> [[[ @maparr ]]]";
 	@srtmaparr = sort {$a <=> $b} @maparr;
-	print "\n -> [[[ @srtmaparr ]]]\n";
-	#Under construction
+	#print " -> [[[ @srtmaparr ]]]\n";
+	@arr = ();
+	foreach(@srtmaparr){
+		($s,$e) = split("-",$_);
+		$s = $s.":s";
+		$e = $e.":e";
+		push(@arr,$s,$e);
+	}
+	@sarr = sort {$a <=> $b} @arr;
+	#foreach(@sarr){
+	#	print "$_\n";
+	#}
+	$lv = 0;
+	foreach(@sarr){
+		chomp;
+		($v,$stat) = split(":",$_);
+		if($stat eq "s"){$lv++;}
+		if($stat eq "e"){$lv--;}
+		#print "$_ $lv\n";
+		if($stat eq "s" && $lv == 1){print "region-complex-Material:$v";}
+		if($stat eq "e" && $lv == 0){print "-$v\n";}
+	}
+	print ";;;\n";
 }
 
 
