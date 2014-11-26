0b0cf0620730d7398daeb822f1aba8f5b2e4a93d
kouamano <amano.au1@gmail.com>
2014-11-26 16:03:15 +0900

    rev

--- splitter2.pl
+++ splitter2.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 
 while(<>){
-	$_ =~ s/[\s\.,:;-_<>\(\)\{\}\[\]]+/ /ig;	#/usr/local/var/perl_word_delimiter.patt.2
-	print $_;
-
+	$_ =~ s/[\s\.,:;\-_<>\(\)\{\}\[\]]+/ /ig;	#/usr/local/var/perl_word_delimiter.patt.2
+	$_ =~s/^\s*//;
+	$_ =~s/\s*$//;
+	print " $_";
 }
