ae2e5342a4559b0ed5e299401fd9c11079928b47
kouamano <amano.au1@gmail.com>
2014-12-04 11:02:34 +0900

    new

---
+++ splitter3.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+while(<>){
+	chomp;
+	$_ =~ s/[\s\.,:;\-_<>\(\)\{\}\[\]\'\"]+/ /ig;	#/usr/local/var/perl_word_delimiter.patt.2
+	$_ =~ s/^\s*//;
+	$_ =~ s/\s*$//;
+	print " $_";
+}
+print "\n";
