016e054ea47895ea78e7d53d8bc9618a0b5561c2
kouamano <amano.au1@gmail.com>
2015-04-01 21:13:32 +0900

    rev

--- add-level_to_sort_pos.pl
+++ add-level_to_sort_pos.pl
@@ -6,9 +6,9 @@ while(<>){
 	if($_ =~ /:s:/){
 		$current_level = $current_level + 1;
 		print "$_\t$current_level\n";
-	}elsif($_ =^ /:e:/){
-		$current_level = $current_level - 1;
+	}elsif($_ =~ /:e:/){
 		print "$_\t$current_level\n";
+		$current_level = $current_level - 1;
 	}else{
 		print "$_\t$current_level\n";
 	}
