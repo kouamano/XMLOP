d35e797f7ec131837d71dbc5ab98985dc4920c69
kouamano <amano.au1@gmail.com>
2015-04-01 23:21:52 +0900

    rev

---
+++ extract_pos_xref_in_body.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+$IN_body = 0;
+$CUR_sec = "";
+$CUR_p_st = "";
+$CUR_p_en = "";
+$CUR_p_lv_st = 0;
+$CUR_p_lv_en = 0;
+$prt_st = 0;
+$prt_en = 0;
+while(<>){
+	chomp;
+	if($_ =~ /\.\//){
+		print "$_\n";
+	}
+	if($_ =~ /:s:<body>:/){
+		$IN_body = 1;
+	}elsif($_ =~ /:e:<\/body>:/){
+		$IN_body = 0;
+	}
+	if($IN_body == 1){
+
+	if($_ =~ /:s:<sec/){
+		$CUR_sec = $_;
+	}
+	if($_ =~ /:s:<p>:/){
+		$CUR_p_st = $_;
+		@tmp = split(/\t/,$CUR_p_st);
+		$CUR_p_lv_st = pop(@tmp);
+	}
+	if($_ =~ /:s:<xref/){
+		if($prt_st == 0){
+			print "$CUR_p_st\n";
+			$prt_st = 1;
+		}
+		print "$_\t";
+		#print "$CUR_p_st\t$_\t";
+	}
+	if($_ =~ /:e:<\/xref>:/){
+		print "$_\n";
+		$prt_en = 1;
+	}
+	if($_ =~ /:e:<\/p>:/){
+		#print "HIT\n";
+		$CUR_p_en = $_;
+		@tmp = split(/\t/,$CUR_p_en);
+		$CUR_p_lv_en = pop(@tmp);
+		if($prt_en == 1){
+			print "$CUR_p_en\n";
+			$prt_en = 0;
+		}
+		$prt_st = 0;
+	}
+
+	}
+	if($_ =~ /;;;/){
+		print "$_\n";
+	}
+}
