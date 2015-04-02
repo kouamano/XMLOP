84e1ceb16ab0deeea2727b9b87ad7c41c89f752a
kouamano <amano.au1@gmail.com>
2015-04-02 09:26:45 +0900

    rev

--- extract_pos_xref_in_body.pl
+++ extract_pos_xref_in_body.pl
@@ -4,14 +4,18 @@ $IN_body = 0;
 $CUR_sec = "";
 $CUR_p_st = "";
 $CUR_p_en = "";
-$CUR_p_lv_st = 0;
-$CUR_p_lv_en = 0;
+#$CUR_p_lv_st = 0;
+#$CUR_p_lv_en = 0;
 $prt_st = 0;
 $prt_en = 0;
+$count = 0;
 while(<>){
 	chomp;
 	if($_ =~ /\.\//){
 		print "$_\n";
+		$prt_st = 0;
+		$prt_en = 0;
+		$count = 0;
 	}
 	if($_ =~ /:s:<body>:/){
 		$IN_body = 1;
@@ -19,40 +23,39 @@ while(<>){
 		$IN_body = 0;
 	}
 	if($IN_body == 1){
-
-	if($_ =~ /:s:<sec/){
-		$CUR_sec = $_;
-	}
-	if($_ =~ /:s:<p>:/){
-		$CUR_p_st = $_;
-		@tmp = split(/\t/,$CUR_p_st);
-		$CUR_p_lv_st = pop(@tmp);
-	}
-	if($_ =~ /:s:<xref/){
-		if($prt_st == 0){
-			print "$CUR_sec\n";
-			print "$CUR_p_st\n";
-			$prt_st = 1;
+		if($_ =~ /:s:<sec/){
+			$CUR_sec = $_;
 		}
-		print "$_\t";
-		#print "$CUR_p_st\t$_\t";
-	}
-	if($_ =~ /:e:<\/xref>:/){
-		print "$_\n";
-		$prt_en = 1;
-	}
-	if($_ =~ /:e:<\/p>:/){
-		#print "HIT\n";
-		$CUR_p_en = $_;
-		@tmp = split(/\t/,$CUR_p_en);
-		$CUR_p_lv_en = pop(@tmp);
-		if($prt_en == 1){
-			print "$CUR_p_en\n";
-			$prt_en = 0;
+		if($_ =~ /:s:<p>:/){
+			$CUR_p_st = $_;
+			#@tmp = split(/\t/,$CUR_p_st);
+			#$CUR_p_lv_st = pop(@tmp);
 		}
-		$prt_st = 0;
-	}
-
+		if($_ =~ /:s:<xref/){
+			if($prt_st == 0){
+				$count++;
+				print "R"."$count\n";
+				print "$CUR_sec\n";
+				print "$CUR_p_st\n";
+				$prt_st = 1;
+			}
+			print "$_\t";
+			#print "$CUR_p_st\t$_\t";
+		}
+		if($_ =~ /:e:<\/xref>:/){
+			print "$_\n";
+			$prt_en = 1;
+		}
+		if($_ =~ /:e:<\/p>:/){
+			$CUR_p_en = $_;
+			#@tmp = split(/\t/,$CUR_p_en);
+			#$CUR_p_lv_en = pop(@tmp);
+			if($prt_en == 1){
+				print "$CUR_p_en\n";
+				$prt_en = 0;
+			}
+			$prt_st = 0;
+		}
 	}
 	if($_ =~ /;;;/){
 		print "$_\n";
