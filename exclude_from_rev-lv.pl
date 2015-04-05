15723d363360d9a4d5ec78df708de678c3561473
kouamano <amano.au1@gmail.com>
2015-04-05 15:20:19 +0900

    rev

--- exclude_from_rev-lv.pl
+++ exclude_from_rev-lv.pl
@@ -4,11 +4,31 @@ $pr = 0;
 while(<>){
 	if($_ =~ /^\.\//){
 		print $_;
+		$pr = 0;
 	}
-	if($_ =~ /^ \[/){
-		print $_;
+	elsif($_ =~ /^ \[/){
+		#print $_;
+		if($_ =~ /.+table.+xref/i){
+			$pr = 0;
+		}elsif($_ =~ /.+fig.+xref/i){
+			$pr = 0;
+		}else{
+			$pr = 1;
+		}
+		if($pr == 1){
+			print $_;
+		}else{
+			#print "RM> $_";
+		}
 	}
-	if($_ =~ /^;;;/){
+	elsif($_ =~ /^[0-9]+/){
+		if($pr == 1){
+			print $_;
+		}else{
+			#print "RM> $_";
+		}
+	}
+	elsif($_ =~ /^;;;/){
 		print $_;
 	}
 }
