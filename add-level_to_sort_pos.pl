70a376d3ae9126f74697b6813fa129bdcc0920de
kouamano <amano.au1@gmail.com>
2015-04-16 15:47:29 +0900

    rev

--- add-level_to_sort_pos.pl
+++ add-level_to_sort_pos.pl
@@ -4,15 +4,19 @@ $current_level = 0;
 while(<>){
 	chomp;
 	if($_ =~ /\.\//){
+		#print "COND:0\n";
 		$current_level = 0;
-	}
-	if($_ =~ /:s:/){
+		print "$_\t$current_level";
+	}elsif($_ =~ /:s:/){
+		#print "COND:1\n";
 		$current_level = $current_level + 1;
 		print "$_\t$current_level\n";
 	}elsif($_ =~ /:e:/){
+		#print "COND:2\n";
 		print "$_\t$current_level\n";
 		$current_level = $current_level - 1;
 	}else{
+		#print "COND:3\n";
 		print "$_\t$current_level\n";
 	}
 }
