0bc1698123f068f037ce5c11114dfd2b0ccc785b
kouamano <amano.au1@gmail.com>
2015-04-17 14:12:42 +0900

    rev

--- xml_COM_to_SP.pl
+++ xml_COM_to_SP.pl
@@ -4,9 +4,12 @@ while(<>){
 	push(@arr,$_);
 }
 $tx = join('',@arr);
-$tx =~ s/\s/ /;
-$tx =~ s/(<!--.*-->)/$count=0;$sb="";while($count < length($1)){$sb = $sb." "; $count++;};$sb/eg;
-print $tx;
+$tx =~ s/\s/ /g;
+#print "$tx\n";
+$tx =~ s/(<!--.*?-->)/$count=0;$sb="";while($count < length($1)){$sb = $sb." "; $count++;};$sb/eg;
+#print "++++++\n";
+#print "$tx\n";
+print "$tx";
 #while(<>){
 #	$_ =~ s/(<!--.*-->)/$count=0;while($count < length($1)){print " "; $count++;}/eg;
 #	print $_;
