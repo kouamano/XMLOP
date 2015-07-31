0c4ae89fea996b9f11bb21f49b84b1997bf6b801
kouamano <amano.au1@gmail.com>
2015-07-31 09:55:14 +0900

    new

---
+++ splitter1.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+while(<>){
+	chomp;
+	$_ =~ s/[\s\.,;<>\(\)\[\]]+/ /ig;	#/usr/local/var/perl_word_delimiter.patt.1
+	$_ =~ s/^\s+//;
+	$_ =~ s/\s+$//;
+	print " $_";
+}
+print "\n";
