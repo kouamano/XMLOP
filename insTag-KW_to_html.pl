2d5242e50353efb57c04e097f107f2eec237a3fc
kouamano <amano.au1@gmail.com>
2016-09-05 13:24:22 +0900

    new

---
+++ insTag-KW_to_html.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+$srcfile=$ARGV[0];
+$patfile=$ARGV[1];
+$tag="KW";
+$tag=$ARGV[2];
+
+open(IN,$patfile);
+while(<IN>){
+	chomp;
+	push(@patarr,$_);
+}
+close(IN);
+
+#print @patarr;
+
+open(IN,$srcfile);
+while(<IN>){
+	$ln = $_;
+	foreach(@patarr){
+		$ln =~ s/([\s\(>])($_)([\s,;\.\)<])/$1<$tag>$2<\/$tag>$3/g;
+		#$ln =~ s/([\s\(])($_)([\s,;\.\)])/$1<ORG>$2<\/ORG>$3/g;
+	}
+	print $ln;
+}
+close(IN);
