f95841316c43dfb1ea9e9ceae1cd5966bc7f9f07
kouamano <amano.au1@gmail.com>
2017-05-11 14:18:42 +0900

    rev

--- extract_element.pl
+++ extract_element.pl
@@ -6,20 +6,25 @@ $op1 = $ARGV[2];
 if ($op1 eq "") {
 	$op1 = "-E"; #Including other tags
 }
+$op2 = $ARGV[3];
+if ($op2 eq "") {
+	$op2 = "\n";
+}
+
 
 open(IN,$f);
 if($op1 eq "-e"){
 	while(<IN>){
 		@hit = $_ =~ /(<${tag}[^<>]*>[^<>]*?<\/$tag>)/g;
 		foreach $j (@hit) {
-			print "$j\n";
+			print "$j$op2";
 		}
 	}
 }elsif($op1 eq "-E"){
 	while(<IN>){
 		@hit = $_ =~ /(<${tag}[^<>]*>.*?<\/$tag>)/g;
 		foreach $j (@hit) {
-			print "$j\n";
+			print "$j$op2";
 		}
 	}
 }else{
