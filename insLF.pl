2e9582ef384c6eafd1f33ca1600ac41d7254d273
kouamano <amano.au1@gmail.com>
2017-08-14 23:49:41 +0900

    add

--- insLF.pl
+++ insLF.pl
@@ -10,7 +10,14 @@ while(<>){
 			chomp;
 			print "\t$_\n";
 		}
-		#print "$_";
+	}elsif($_ =~ /^b\"/){
+		$_ =~ s/^b\"//;
+		$_ =~ s/\"$//;
+		@arr = split(/<;\/>/,$_);
+		foreach(@arr){
+			chomp;
+			print "\t$_\n";
+		}
 	}else{
 		print "$_\n";
 	}
