993a7de0f8b8a8f8a503a4a29f7a8f4fff9303cf
kouamano <amano.au1@gmail.com>
2022-08-29 21:47:26 +0900

    rev

--- elementTagPos.pl
+++ elementTagPos.pl
@@ -25,23 +25,23 @@ while($tx =~ /(<[^<>]*[^\/]{0,1}>)/ig){
 	$e = $p-1;
 	#push(@arr,"$s"."-"."$e"."::$target:\n");
 	if($target =~ /^<!/){
-		push(@arr,"$s"."-"."$e".":"."i".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."i"."\t:$target:\n");
 	}elsif($target =~ /^<\?/){
-		push(@arr,"$s"."-"."$e".":"."i".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."i"."\t:$target:\n");
 	}elsif($target =~ /^<>$/){
-		push(@arr,"$s"."-"."$e".":"."n".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."n"."\t:$target:\n");
 	}elsif($target =~ /^<\/>$/){
-		push(@arr,"$s"."-"."$e".":"."n".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."n"."\t:$target:\n");
 	}elsif($target =~ /^<\//){
-		push(@arr,"$s"."-"."$e".":"."e".":$target:\t$lv\n");
+		push(@arr,"$s"."-"."$e".":"."e"."\t:$target:\t$lv\n");
 		$lv--;
 	}elsif($target =~ /^<[^<>]+\/>$/){
-		push(@arr,"$s"."-"."$e".":"."a".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."a"."\t:$target:\n");
 	}elsif($target =~ /^<[^<>\/][^<>]*>$/){
 		$lv++;
-		push(@arr,"$s"."-"."$e".":"."s".":$target:\t$lv\n");
+		push(@arr,"$s"."-"."$e".":"."s"."\t:$target:\t$lv\n");
 	}else{
-		push(@arr,"$s"."-"."$e".":"."o".":$target:\n");
+		push(@arr,"$s"."-"."$e".":"."o"."\t:$target:\n");
 	}
 	#if($target =~ /\/>$/){
 	#	push(@arr,"$s-$e:a:$target:\n");
